69601c216497394cb6101cfa105ce23832a841f3
abheeacharya2019 <abhiacharya8@gmail.com>
2019-06-08 23:28:39 +0530

    Adding all Script file on remote repository

---
+++ Hello4.sh
@@ -0,0 +1,6 @@
+# ! /bin/bash
+read -p "What is your name ?" name
+echo "Hello $name"
+read -n1 -p "Press any key to exit"
+echo
+exit 0
---
+++ Hello5.sh
@@ -0,0 +1,6 @@
+# ! /bin/bash
+read -p "What is your name ?" name
+echo "Hello $name"
+read -sn2 -p "Press any key for exit...."
+echo
+exit 0
